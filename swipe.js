f3c02d702d741f8871060b418b2eb0cc68ee6625
Jono Cooper <jono@jonocooper.com>
2016-10-09 23:31:28 +1300

    Add iOS Back Gesture

--- swipe.js
+++ swipe.js
@@ -11,6 +11,7 @@ var swipe = {
   contentEl: null,
   lock: null,
   longTouch: false,
+  iOSBack: null,
 
   // in react or probably anything else, you would just bind
   // all these events using JSX, rather than this function
@@ -18,6 +19,8 @@ var swipe = {
     swipe.containerEl = elem
     swipe.headerEl =  elem.querySelector('.swipe-header')
     swipe.contentEl = elem.querySelector('.swipe-content')
+
+    swipe.iOSBack = swipe.containerEl
 
     swipe.contentEl.addEventListener('touchstart', swipe.contentTouchStart)
     swipe.contentEl.addEventListener('touchmove', swipe.contentTouchMove)
@@ -71,6 +74,9 @@ var swipe = {
     swipe.lastXPos = event.touches[0].pageX
     swipe.lastYPos = event.touches[0].pageY
     swipe.contentEl.className = swipe.contentEl.className.replace(' swipe-animate', '')
+    if (swipe.iOSBack !== null) {
+      swipe.iOSBack.className = swipe.iOSBack.className.replace(' swipe-animate', '')
+    }
     var swipeBar = swipe.headerEl.querySelector('.swipe-bar')
     swipeBar.className = swipeBar.className.replace(' swipe-animate', '')
   },
@@ -82,7 +88,11 @@ var swipe = {
       if (swipe.lastYPos > event.touches[0].pageY + thresh || swipe.lastYPos < event.touches[0].pageY - thresh) {
         swipe.lock = 'y'
       } else {
-        swipe.lock = 'x'
+        if (swipe.iOSBack !== null && swipe.touchStartPos - swipe.iOSBack.getBoundingClientRect().left < 14) {
+          swipe.lock = 'ios'
+        } else {
+          swipe.lock = 'x'  
+        }        
       }
     }
     if (swipe.lock === 'x') {
@@ -95,33 +105,59 @@ var swipe = {
       swipe.headerEl
         .querySelector('.swipe-bar')
         .setAttribute('style', 'width:' + swipe.headerEl.children[0].children[0].offsetWidth + 'px;transform:translate('+ slideWidth + 'px)')
+    } else if (swipe.lock === 'ios') {
+      event.preventDefault()
+      var pos = (swipe.index * swipe.slideWidth) - swipe.moveX
+      if (pos < 0) {
+        pos = 0
+      }
+      swipe.iOSBack.setAttribute('style', 'transform: translate3d('+pos+'px,0,0)')
     }
   },
   contentTouchEnd: function(event) {
-    if (swipe.lock === 'y') {
-      swipe.lock = null
-      return
-    }
-    swipe.headerEl.children[0].children[swipe.index].className = swipe.headerEl.children[0].children[swipe.index].className.replace(' active', '')
-    var absMove = Math.abs(swipe.index * swipe.slideWidth - swipe.moveX)
-    if (absMove > swipe.slideWidth/2 || swipe.longTouch === false) {
-      // rejects touches that don't really move
-      if (Math.abs(swipe.touchStartPos - swipe.lastXPos) > 3) {
-        // determines which way the swipe was
-        if (swipe.moveX > swipe.index * swipe.slideWidth && swipe.index < swipe.slideNumber) {
-          swipe.index++
-        } else if (swipe.moveX < swipe.index * swipe.slideWidth && swipe.index > 0) {
-          swipe.index--
+    // if (swipe.lock === 'y') {
+    //   return
+    // }
+    if (swipe.lock === 'x') {
+      swipe.headerEl.children[0].children[swipe.index].className = swipe.headerEl.children[0].children[swipe.index].className.replace(' active', '')
+
+      var absMove = Math.abs(swipe.index * swipe.slideWidth - swipe.moveX)
+      if (absMove > swipe.slideWidth/2 || swipe.longTouch === false) {
+        // rejects touches that don't really move
+        if (Math.abs(swipe.touchStartPos - swipe.lastXPos) > 3) {
+          // determines which way the swipe was
+          if (swipe.moveX > swipe.index * swipe.slideWidth && swipe.index < swipe.slideNumber) {
+            swipe.index++
+          } else if (swipe.moveX < swipe.index * swipe.slideWidth && swipe.index > 0) {
+            swipe.index--
+          }
         }
       }
+      swipe.contentEl.className = swipe.contentEl.className + ' swipe-animate'
+      swipe.contentEl.setAttribute('style', 'transform: translate3d(-'+swipe.index*swipe.slideWidth+'px,0,0)')
+      
+      swipe.headerEl.children[0].children[swipe.index].className += ' active'
+      var swipeBar = swipe.headerEl.querySelector('.swipe-bar')
+      swipeBar.className = swipeBar.className + ' swipe-animate'
+      swipeBar.setAttribute('style', 'width:' + swipe.headerEl.children[0].children[0].offsetWidth + 'px;transform:translate3d('+swipe.index*swipe.headerEl.children[0].children[0].offsetWidth + 'px,0,0)')
+
+    } else if (swipe.lock === 'ios') {
+      var swipedAway = false
+      var absMove = Math.abs(swipe.index * swipe.slideWidth - swipe.moveX)
+      if (absMove > swipe.slideWidth/2 || swipe.longTouch === false) {
+        // rejects touches that don't really move
+        if (Math.abs(swipe.touchStartPos - swipe.lastXPos) > 3) {
+          swipedAway = true
+        }
+      }
+      swipe.iOSBack.className = swipe.iOSBack.className + ' swipe-animate'
+      if (swipedAway) {
+        swipe.iOSBack.setAttribute('style', 'transform: translate3d('+swipe.slideWidth+'px,0,0)')
+      } else {
+        swipe.iOSBack.setAttribute('style', 'transform: translate3d(0px,0,0)')
+      }
     }
-    swipe.contentEl.className = swipe.contentEl.className + ' swipe-animate'
-    swipe.contentEl.setAttribute('style', 'transform: translate3d(-'+swipe.index*swipe.slideWidth+'px,0,0)')
     swipe.lock = null
-    swipe.headerEl.children[0].children[swipe.index].className += ' active'
-    var swipeBar = swipe.headerEl.querySelector('.swipe-bar')
-    swipeBar.className = swipeBar.className + ' swipe-animate'
-    swipeBar.setAttribute('style', 'width:' + swipe.headerEl.children[0].children[0].offsetWidth + 'px;transform:translate3d('+swipe.index*swipe.headerEl.children[0].children[0].offsetWidth + 'px,0,0)')
   }
 }
 if (typeof exports !== 'undefined') {
